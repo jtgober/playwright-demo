c006a622cc8ff7c29128c8f210d148d5396a3beb
Jonathan Gober <gober.jonathan@gmail.com>
2023-02-27 19:05:23 -0500

    adding env vars

--- tests/sample-app.spec.js
+++ tests/sample-app.spec.js
@@ -1,14 +1,14 @@
 const { test } = require('@playwright/test');
 const { SampleAppPage } = require('../models/sample-app.model');
-
+let username = process.env.USERNAME
+let password = process.env.PASSWORD
 test.describe.parallel('suite', () => {
-
 
     test('log in success', async ({ page }) => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
-        await sampleAppPage.fillUsernameField("Gojo")
-        await sampleAppPage.fillPasswordField("pwd")
+        await sampleAppPage.fillUsernameField(username)
+        await sampleAppPage.fillPasswordField(password)
         await sampleAppPage.clickLoginOrLogoutButton()
         await sampleAppPage.expectedLoginTextToBe("Welcome, Gojo!")
     });
@@ -34,8 +34,8 @@ test.describe.parallel('suite', () => {
     test('log out test', async ({ page }) => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
-        await sampleAppPage.fillUsernameField("Gojo")
-        await sampleAppPage.fillPasswordField("pwd")
+        await sampleAppPage.fillUsernameField(username)
+        await sampleAppPage.fillPasswordField(password)
         await sampleAppPage.clickLoginOrLogoutButton()
         await sampleAppPage.expectedLoginTextToBe("Welcome, Gojo!")
         await sampleAppPage.clickLoginOrLogoutButton()
