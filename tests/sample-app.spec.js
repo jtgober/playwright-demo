7e4ae804d698a3bb8e1b3f858971b76d7a9edad8
Jonathan Gober <gober.jonathan@gmail.com>
2023-03-05 19:27:06 -0500

    reverting to starter code

--- tests/sample-app.spec.js
+++ tests/sample-app.spec.js
@@ -1,22 +1,22 @@
 const { test } = require('@playwright/test');
 const { SampleAppPage } = require('../models/sample-app.model');
-let username = process.env.USERNAME
-let password = process.env.PASSWORD
+
 test.describe.parallel('suite', () => {
+
 
     test('log in success', async ({ page }) => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
-        await sampleAppPage.fillUsernameField(username)
-        await sampleAppPage.fillPasswordField(password)
+        await sampleAppPage.fillUsernameField("Gojo")
+        await sampleAppPage.fillPasswordField("pwd")
         await sampleAppPage.clickLoginOrLogoutButton()
-        await sampleAppPage.expectedLoginTextToBe(`Welcome, ${username}!`)
+        await sampleAppPage.expectedLoginTextToBe("Welcome, Gojo!")
     });
 
     test('Wrong password test', async ({ page }) => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
-        await sampleAppPage.fillUsernameField(username)
+        await sampleAppPage.fillUsernameField("Gojo")
         await sampleAppPage.fillPasswordField("WrongPass")
         await sampleAppPage.clickLoginOrLogoutButton()
         await sampleAppPage.expectedLoginTextToBe("Invalid username/password")
@@ -26,7 +26,7 @@ test.describe.parallel('suite', () => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
         await sampleAppPage.fillUsernameField("")
-        await sampleAppPage.fillPasswordField(password)
+        await sampleAppPage.fillPasswordField("pwd")
         await sampleAppPage.clickLoginOrLogoutButton()
         await sampleAppPage.expectedLoginTextToBe("Invalid username/password")
     });
@@ -34,10 +34,10 @@ test.describe.parallel('suite', () => {
     test('log out test', async ({ page }) => {
         const sampleAppPage = new SampleAppPage(page)
         await sampleAppPage.navigateToSampleApp()
-        await sampleAppPage.fillUsernameField(username)
-        await sampleAppPage.fillPasswordField(password)
+        await sampleAppPage.fillUsernameField("Gojo")
+        await sampleAppPage.fillPasswordField("pwd")
         await sampleAppPage.clickLoginOrLogoutButton()
-        await sampleAppPage.expectedLoginTextToBe(`Welcome, ${username}!`)
+        await sampleAppPage.expectedLoginTextToBe("Welcome, Gojo!")
         await sampleAppPage.clickLoginOrLogoutButton()
         await sampleAppPage.expectedLoginTextToBe("User logged out.")
     });
